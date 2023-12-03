2d9859851439babec77bbd21c8a37cd65e58b616
cjeanm11 <77751904+cjeanm11@users.noreply.github.com>
2023-12-03 01:14:42 -0500

    file refractor

--- src/reader/buffer.go
+++ src/reader/buffer.go
@@ -2,18 +2,18 @@ package reader
 
 import (
 	"encoding/binary"
-	"go_log_reader/src/enums"
+	"go_log_reader/src/enums/errors"
 	"sync"
 )
 
 func ReadSize(buf *[]byte, offset int) (uint32, error) {
 	if len(*buf) < offset+8 {
-		return 0, enums.ErrInvalidData
+		return 0, errors.ErrInvalidData
 	}
 
 	segments := binary.LittleEndian.Uint32((*buf)[offset:])
 	if len(*buf) < offset+4+int(segments*4) {
-		return 0, enums.ErrInvalidData
+		return 0, errors.ErrInvalidData
 	}
 
 	var mu sync.Mutex
@@ -31,7 +31,7 @@ func ReadSize(buf *[]byte, offset int) (uint32, error) {
 
 			if len(*buf) < offset+idx+4 {
 				// Handle error condition
-				err = enums.ErrInvalidData
+				err = errors.ErrInvalidData
 				return
 			}
 			segSize := binary.LittleEndian.Uint32((*buf)[offset+idx:])
@@ -58,7 +58,7 @@ func ReadMessage(buf *[]byte, offset int) ([]byte, error) {
 	}
 
 	if size == 0 || len(*buf) < offset+int(size) {
-		return nil, enums.ErrInsufficientData
+		return nil, errors.ErrInsufficientData
 	}
 	return (*buf)[offset : offset+int(size)], nil
 }
